532b1abab4d5ea9f6cb2493461a40a979614cf0b
Wallace Tavares <108297008+Wfelipetm@users.noreply.github.com>
2025-03-27 10:38:53 -0300

    .
    .

--- servidor/app/services/mail.py
+++ servidor/app/services/mail.py
@@ -7,6 +7,6 @@ def init_mail(app):
     app.config['MAIL_PORT'] = 587
     app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USE_SSL'] = False
-    app.config['MAIL_DEFAULT_SENDER'] = 'biometrico@itaguai.rj.gov.br'
+    app.config['MAIL_DEFAULT_SENDER'] = ('MARCAÇÃO DE PONTO', 'naoresponda@itaguai.rj.gov.br')
 
     mail.init_app(app)
