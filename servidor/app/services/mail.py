cdc1760d9ad45b56e909a72d28725f0dbf59fbac
Wallace Tavares <108297008+Wfelipetm@users.noreply.github.com>
2025-03-31 15:18:09 -0300

    .
    .

--- servidor/app/services/mail.py
+++ servidor/app/services/mail.py
@@ -1,12 +1,16 @@
+import os
 from flask_mail import Mail
+from dotenv import load_dotenv
+
+load_dotenv()  # Carrega as variáveis do .env
 
 mail = Mail()
 
 def init_mail(app):
-    app.config['MAIL_SERVER'] = 'smtp-apl.serpro.gov.br'
-    app.config['MAIL_PORT'] = 587
-    app.config['MAIL_USE_TLS'] = True
-    app.config['MAIL_USE_SSL'] = False
-    app.config['MAIL_DEFAULT_SENDER'] = ('MARCAÇÃO DE PONTO', 'naoresponda@itaguai.rj.gov.br')
+    app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
+    app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT', 587))  # Converte para int
+    app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS', 'True') == 'True'  # Converte para bool
+    app.config['MAIL_USE_SSL'] = os.getenv('MAIL_USE_SSL', 'False') == 'True'
+    app.config['MAIL_DEFAULT_SENDER'] = (os.getenv('MAIL_DEFAULT_SENDER_NAME'), os.getenv('MAIL_DEFAULT_SENDER_EMAIL'))
 
     mail.init_app(app)
