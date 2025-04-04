bc262320426bc0bbf7cef7a2d8f05186272c57d5
Wallace Tavares <108297008+Wfelipetm@users.noreply.github.com>
2025-04-04 11:47:49 -0300

    Revert "."
    This reverts commit a1578617c273ce50fb4e0d4c6db21e8b3361e88a.

--- servidor/server.py
+++ servidor/server.py
@@ -1,19 +1,13 @@
 from app.routes import create_app
-from app.db.database import get_db_connection
-from flask_cors import CORS
-import os
+from app.db.database import get_db_connection  
+from app.services.mail import mail, init_mail
 
-# Criação da aplicação Flask
 app = create_app()
+init_mail(app)
 
-# Ativando CORS somente para o domínio específico e com suporte a cookies
-CORS(
-    app,
-    supports_credentials=True,
-    origins=["https://prefeitura.itaguai.rj.gov.br"]
-)
 
-# Verificando conexão com o banco de dados
+
+
 conn = get_db_connection()
 if conn:
     print("Conectado ao banco de dados com sucesso!")
@@ -22,14 +16,4 @@ else:
     print("Falha ao conectar ao banco de dados.")
 
 if __name__ == '__main__':
-    # Certificados SSL
-    cert_path = os.path.abspath('C:\\https\\cert.pem')
-    key_path = os.path.abspath('C:\\https\\key.pem')
-
-    # Executando a aplicação com SSL
-    app.run(
-        debug=True,
-        host='0.0.0.0',
-        port=5000,
-        ssl_context=(cert_path, key_path)
-    )
+    app.run(debug=True, host='0.0.0.0', port=5000)
