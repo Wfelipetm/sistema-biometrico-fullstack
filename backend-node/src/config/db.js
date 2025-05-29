d5e72d4f02a9d1c6a236d84341484a68702a9a3b
Wallace Tavares <108297008+Wfelipetm@users.noreply.github.com>
2025-05-29 11:59:00 -0300

    .
    .

--- backend-node/src/config/db.js
+++ backend-node/src/config/db.js
@@ -2,13 +2,11 @@ const { Pool } = require('pg');
 
 // Configuração da conexão com o PostgreSQL
 const pool = new Pool({
-  host: 'biometrico.itaguai.rj.gov.br',
-  // host: 'localhost',
+  host: 'localhost',
   port: 5432,
   database: 'biometrico',
   user: 'postgres',
-  //password: '123123',
-  password: 'B10m3Tr1@',
+  password: '123123',
 });
 
 pool.on('connect', () => {
