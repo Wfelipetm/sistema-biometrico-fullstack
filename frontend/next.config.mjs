882dc2d6940f5c144c475a2ade72428ede569682
RonysonSilva <ronyson.silva96@gmail.com>
2025-05-29 09:35:57 -0300

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -9,6 +9,12 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  experimental: {
+    allowedDevOrigins: [
+      'http://10.200.200.10',
+      'http://10.200.200.22',
+    ], // substitua pelo IP ou domínio necessário
+  },
 }
 
 export default nextConfig
